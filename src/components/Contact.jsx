dace0da73398633dab7bb05c1480d7737f30f974
umeshmahato28 <mahatoumesh94@gmail.com>
2024-09-02 11:34:35 +0530

    build contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <input type="name" placeholder='Name' />
+      <input type="gmail" placeholder='Gmail' />
+      <input type="text" placeholder='Type messege here..' />
+    </div>
   )
 }
 
